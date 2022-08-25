61fdcde8e96a4f45f2b234ae9576bc1884ca0bbb
ann <dorisann.me@gmail.com>
2022-08-25 21:46:04 +0000

    added a loading img

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 //listen for submit
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function (e) {
+  document.getElementById("results").style.display = "none";
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 //calculate results
-function calculateResults(e) {
+function calculateResults() {
   console.log("calculating....");
 
   //DOM
@@ -15,27 +20,33 @@ function calculateResults(e) {
   const totalPayment = document.getElementById("total-payment");
   const totalInterest = document.getElementById("total-interest");
 
-  const principal = parseFloat(amount.value) * 12;
-  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-  const calculatedPayments = parseFloat(years.value) * 12;
+  //assign input values
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100;
+  const calculatedPayments = parseFloat(years.value);
 
   //compute monthly payment
-  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-  const monthly = (principal * x * calculatedInterest) / (x - 1);
+  const simpleInterest = principal * calculatedInterest * calculatedPayments;
+  const monthly = simpleInterest / 12;
 
   if (isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
-    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+    totalPayment.value =
+      simpleInterest / (calculatedInterest * calculatedPayments).toFixed(2);
+    totalInterest.value = simpleInterest.toFixed(2);
+
+    document.getElementById("results").style.display = "block";
+    document.getElementById("loading").style.display = "none";
   } else {
     showError("Please check your numbers");
   }
-
-  e.preventDefault();
 }
 
 //show error message
 function showError(error) {
+  document.getElementById("results").style.display = "block";
+  document.getElementById("loading").style.display = "none";
+
   const errorDiv = document.createElement("div");
   errorDiv.className = "alert alert-danger";
   errorDiv.appendChild(document.createTextNode(error));
